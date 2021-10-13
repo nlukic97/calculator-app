6e9256c74b9ebb09f16b20f8d2f34e0b2a28eba0
nlukic97 <nlukic97@gmail.com>
2021-10-13 14:27:02 +0200

    made some fixes with the script

--- public/js/script.js
+++ public/js/script.js
@@ -17,13 +17,13 @@ function newNumber(number){
 //returns true if the last character is an integer (specifically, if it is not a mathemathical operation or a dot/comma)
 function isLastCharInteger(string){
     let char = string[string.length-1]
-    return (char == '-' || char == '+' || char == '*' || char == '/' || char == '.')? false : true;
+    return (char == '-' || char == '+' || char == '*' || char == '/' || char == '.') ? false : true;
 }
 
 // function 
 function calculate(givenNumbers){
     //will not execute if user presses = before entering anything
-    if(givenNumbers === '') return null
+    if(givenNumbers === '') return '';
     
     //removing all the hanging symbols from the end in case there are any (just in case, maybe it's not necessary)
     while(isLastCharInteger(givenNumbers) === false){ 
@@ -37,28 +37,25 @@ function removeLastChar(string){
     return string.substring(0,string.length-1)
 }
 
+function updateDashboard(querySelector, value){
+    if(value != '' && value != null){
+        value.replaceAll('*','x')
+    }
+    document.querySelector(querySelector).innerText = value
+}
 
+function isFormulaEmpty(sring){
+    return (string === '' || string === null) ? true : false;
+}
 
-// Functionalities
-/*
-1. When I press +, it must add a plus to the string. If I press -, it will add a minus
-2. If I press a +, and then a - straight away, it will change the character from + to - (but it doesn't have to be so)
-3. same thing goes for all for signs (*, /, +, and -)
-4. The string I am saving will be the string the user sees
-5. pressing del deletes the last item.
-6. if i press equals, it will make sure that there is no sybmol at the end, and return the calculation
-7. the first element must be a number, or a - sign
-8. maybe remove everything after the last number ?
-9. delete (remove lastSymbol)
-*/
 
 function addListeners(){
     let btns = document.querySelectorAll('.calc-buttons .btn')
     
     btns.forEach(btn=>{
         btn.addEventListener('click',function(){
-            // let check = parseInt()
             let char = this.innerText
+
             if(!isNaN(char)){
                 newNumber(char)
 
@@ -79,8 +76,17 @@ function addListeners(){
                 formula = ''
 
             }
+
+            updateDashboard('.dashboard-text', formula)
         })
     })
 }
 
+
+//init
 addListeners()
+
+/*
+- the first thing a user types cannot be / or x(*)
+- the last thing cannot be a symbol
+*/
